bdd56ab9a4b079473e32e5ce1a7770cf01d1ca26
kipe <tkrs23280810@gmail.com>
2020-02-19 16:39:42 +0900

    finish chapter3

--- thanks.php
+++ thanks.php
@@ -22,6 +22,14 @@
 	print '』<br>';
 	print $email;
 	print 'にメールを送りましたのでご確認ください。';
+
+	$mail_sub = 'アンケート受け付けました。';
+	$mail_body = $nickname."様へ\nアンケートご協力ありがとうございました。";
+	$mail_body = html_entity_decode($mail_body, ENT_QUOTES, "UTF-8");
+	$mail_head = 'From: tkrs23280810@gmail.com';
+	mb_language('Japanese');
+	mb_internal_encoding("UTF-8");
+	mb_send_mail($email, $mail_sub, $mail_body, $mail_head);
 	?>
 
 </body>
